cbed29077f98cc5b88e257dcf3cda3c53a03303b
ASDUBE <ashutoshd6198@gmail.com>
2023-10-09 22:59:32 +0530

    basics of js

---
+++ 01basics/01variables.js
@@ -0,0 +1,9 @@
+const accountId = 14324
+let accountEmail = "hitesh@"
+
+var accountPassword = "12345"
+
+// accountCity = "Mum" not a good practice but can be declared 
+
+console.log(accountId);
+console.table([accountEmail, accountCity]);
---
+++ 01basics/02dataTypes.js
@@ -0,0 +1,16 @@
+"use strict "  // treat all Js code as newer version
+
+// alert (3+3) // node mein use nhi karte only browser
+
+let name ="ASHU"
+let age = 33
+let isLogged = true
+
+/* all data types 
+
+number , bigint , string , boolean , null , undefined , symbol (unique id dene ke liye ),  
+object
+
+*/
+
+console.log(typeof (age));
