b641c413e625bfa48c46d671d92e555e1df64cde
ASDUBE <ashutoshd6198@gmail.com>
2023-10-09 22:59:49 +0530

    basics of  js

---
+++ 01basics/03conversion.js
@@ -0,0 +1,54 @@
+let score = "123"
+
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof(valueInNumber));
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+
+console.log(typeof
+  (booleanIsLoggedIn));
+
+  // 1 = true , 0 = false
+  //" " = false
+  // "ashu" = true
+
+  let number1 = 33
+
+  let string1= String(number1);
+  // console.log(typeof(string1));
+  // console.log(string1);
+
+
+  // operations 
+
+  // console.log("2"+"2"-"2"); // 20
+  // console.log("hello " +"Ashu"); // hello Ashu
+  // console.log(1+"2"+2); // 122
+  // console.log(1+2+"2"); // 32
+
+  // console.log(+true);
+
+  let n1 , n2 , n3 
+
+  n1= n2 = n3 = 2+2 
+
+  console.table([n1, n2,n3])
+
+  
+
+
+
+
+
---
+++ 01basics/04comparison.js
@@ -0,0 +1,13 @@
+console.log(2>1);
+
+console.log("02" >1);
+
+console.log (null >= 0) // true >= works differently becoz > converts null to no. 0 
+console.log(null > 0 ); // false
+
+
+// === strict check 
+
+console.log("2" === 2); // false  datatype same nhi toh equal nhi
+console.log("2" == 2); // true 
+
